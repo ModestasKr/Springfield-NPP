911ca0f46da139a087ef24c30007e74d789283fa
DESKTOP-T4GRT0E\Paulius <pauliusb01@gmail.com>
2022-04-26 10:07:00 +0300

    isfixinta backe useControllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -180,6 +180,14 @@ exports.deleteUserIncome = async (req, res) => {
     res.status(200).json({
       status: "success",
       data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 
 // Add user income
 exports.addToUserIncome = async (req, res) => {
