6d816ebbd5df7b9c01710381d4eb0ec44b90ed15
rokas <dailyuseracc1@gmail.com>
2022-04-28 13:55:41 +0300

    added

--- frontend/src/components/history/HistoryTable.js
+++ frontend/src/components/history/HistoryTable.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import EditExpensesHistoryForm from "./EditExpensesHistoryForm";
 import EditIncomeHistoryForm from "./EditIncomeHistoryForm";
 
-
 import "./style/TableHistory.css";
-
 
 function HistoryTable({
   userId,
@@ -20,62 +18,68 @@ function HistoryTable({
 }) {
   const dateWithoutZeros = date.toString().substr(0, 10);
   const [editFormStatus, setEditFormStatus] = useState(false);
-  
+  let colorClass = (str) => {
+    if (str === "income") {
+      return "table-income";
+    } else {
+      return "table-expense";
+    }
+  };
   return (
-    <>
-    <tr>
-    <td>{dateWithoutZeros}</td>
-      <td>{amount}</td>
-      <td>{category}</td>
-      <td>{name}</td>
-      <td>{dateFixed}</td>
-      <td>{type}</td>
-      <td>
-        <button type="button" onClick={() => deleteItem(userID, subID, type)}>
-          Ištrinti
-        </button>
-      </td>
-      <td>
-        <button
+    <div>
+      <tr>
+        <td className={colorClass(type)}>{amount}</td>
+        <td>{category}</td>
+        <td>{name}</td>
+        <td>{dateWithoutZeros}</td>
+        <td>{type}</td>
+        <td>
+          <button type="button" onClick={() => deleteItem(userID, subID, type)}>
+            Ištrinti
+          </button>
+        </td>
+        <td>
+          <button
             className="button"
             onClick={() => setEditFormStatus(!editFormStatus)}
-          >Update
-        </button>
+          >
+            Update
+          </button>
         </td>
-    </tr>
-    <tr>
-    {editFormStatus && type === "income" && (
-      <EditIncomeHistoryForm
-        getAllUsersData={getAllUsersData}
-        key={subID}
-        id = {subID}
-        category ={category}
-        amount = {amount}
-        editFormStatus={editFormStatus}
-        setEditFormStatus={setEditFormStatus}
-        deleteItem ={deleteItem}
-        type={type}
-        date={date}
-        userId={userId}
-      />
-    )}
-    {editFormStatus && type === "expenses" && (
-      <EditExpensesHistoryForm
-      getAllUsersData={getAllUsersData}
-      key={subID}
-      id = {subID}
-      category ={category}
-      amount = {amount}
-      editFormStatus={editFormStatus}
-      setEditFormStatus={setEditFormStatus}
-      deleteItem ={deleteItem}
-      type={type}
-      date={date}
-      userId={userId}
-      />
-    )}
-  </tr>
-  </>
+      </tr>
+      <tr>
+        {editFormStatus && type === "income" && (
+          <EditIncomeHistoryForm
+            getAllUsersData={getAllUsersData}
+            key={subID}
+            id={subID}
+            category={category}
+            amount={amount}
+            editFormStatus={editFormStatus}
+            setEditFormStatus={setEditFormStatus}
+            deleteItem={deleteItem}
+            type={type}
+            date={date}
+            userId={userId}
+          />
+        )}
+        {editFormStatus && type === "expenses" && (
+          <EditExpensesHistoryForm
+            getAllUsersData={getAllUsersData}
+            key={subID}
+            id={subID}
+            category={category}
+            amount={amount}
+            editFormStatus={editFormStatus}
+            setEditFormStatus={setEditFormStatus}
+            deleteItem={deleteItem}
+            type={type}
+            date={date}
+            userId={userId}
+          />
+        )}
+      </tr>
+    </div>
   );
 }
 
